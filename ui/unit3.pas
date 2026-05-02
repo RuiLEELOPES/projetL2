104d92b5f328db12d31b118c5e38a8bd7da9c23d
RuiLEELOPES <leelopes06@gmail.com>
2026-05-03 01:32:47 +0200

    L'interface version 0.2 avec une première carte

--- ui/unit3.pas
+++ ui/unit3.pas
@@ -5,14 +5,20 @@ unit Unit3;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  Grids, Buttons;
 
 type
 
   { TForm3 }
 
   TForm3 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+
+
+
   private
 
   public
@@ -29,9 +35,26 @@ uses Unit2, Unit1 ;
 {$R *.lfm}
 
 { TForm3 }
+var
+  UNO : TBitmap ;
 
 procedure TForm3.FormCreate(Sender: TObject);
 begin
+
+end;
+
+procedure TForm3.Button1Click(Sender: TObject);
+var
+  CarteW, CarteH : integer ;
+  SrcRect, DestRect : TRect ;
+begin
+  UNO := TBitmap.Create ;
+  UNO.LoadFromFile('C:\Users\User\Downloads\ui\Uno_cards_v2.bmp');
+  CarteW := UNO.Width div 13 ;
+  CarteH := UNO.Height div 5 ;
+  SrcRect := Rect(12 * CarteW, 1 * CarteH, (12 + 1) * CarteW, (1 + 1) * CarteH);
+  DestRect := Rect(70, 50, 70 + CarteW, 50 + CarteH);
+  Canvas.CopyRect(DestRect, UNO.Canvas, SrcRect);
 
 end;
 
